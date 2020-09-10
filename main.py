489d1a77876a3695393685c34dc724874508165e
Alexandre Morignot <amorignot@meilleursagents.com>
2020-09-10 14:33:33 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import json
+from os import environ
+
+from git import Repo
+from github import Github
+
+
+CONFIG = {
+    "main.py": "MAIN",
+    "toto": "you change toto you motherfucker",
+}
+
+
+event = json.load(environ["GITHUB_EVENT_PATH"])
+github = Github(environ["GITHUB_TOKEN"])
+gh_repo = github.get_repo(event["repository"]["full_name"])
+gh_pr = gh_repo.get_pull(event["number"])
+
+
+repo = Repo(".")
+for diff in repo.head.commit.diff(repo.commit("master")):
+    for path in CONFIG:
+        if diff.a_path.startswith(path) or diff.b_path.startswith(path):
+            gh_pr.create_comment(CONFIG["path"])
